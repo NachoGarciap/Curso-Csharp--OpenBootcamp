ce499872b5dff9df672ae3e31e7b77ce7a8d9393
Nacho <igp0003@alu.medac.es>
2023-03-09 01:59:53 +0100

    ejercicios resueltos

---
+++ EjerciciosSesion3/EjerciciosSesion3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //instanciamos el objeto cliente
+        Cliente Cliente = new Cliente();
+        Console.Write("Escribe tu nombre completo: ");
+        Cliente.nombreCompleto = Console.ReadLine();
+
+        Console.Write("Escribe tu telefono: ");
+        Cliente.telefono = Console.ReadLine();
+
+        Console.Write("Escribe tu direccion: ");
+        Cliente.direccion = Console.ReadLine();
+
+        Console.Write("Escribe tu email: ");
+        Cliente.email = Console.ReadLine();
+
+        Cliente.nuevoCliente = true;//true= si, false =no
+        
+
+        Console.WriteLine("");
+        Cliente.MostrarDatos();
+    }
+}
+
+//Estructura cliente
+struct Cliente
+{
+    //campos
+    public string nombreCompleto;
+    public string telefono;
+    public string direccion;
+    public string email;
+    public bool nuevoCliente; //true= si, false =no
+
+    //Metodo Mostrar datos
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Nombre completo: " +nombreCompleto);
+        Console.WriteLine($"Teléfono: "+ telefono);
+        Console.WriteLine($"Dirección: "+direccion);
+        Console.WriteLine($"Email: "+email);
+        Console.WriteLine($"¿Es nuevo cliente? (si/no): "+nuevoCliente);
+    }
+}
+
+
